8fc653ff155e30e0088ba088b08bfc46f7591880
tonyjeffferson <31293930+tonyjeffferson@users.noreply.github.com>
2025-08-24 13:18:43 -0300

    Update lsw-refresh.sh

--- src/lsw-refresh.sh
+++ src/lsw-refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fsSL https://raw.githubusercontent.com/winapps-org/winapps/main/setup.sh | bash
+curl -fsSL https://raw.githubusercontent.com/tonyjeffferson/lsw/refs/heads/main/src/winapps/main/setup.sh | bash
 sleep 1
-curl -fsSL https://raw.githubusercontent.com/winapps-org/winapps/main/setup.sh | bash
+curl -fsSL https://raw.githubusercontent.com/tonyjeffferson/lsw/refs/heads/main/src/winapps/main/setup.sh | bash
 exit 0
